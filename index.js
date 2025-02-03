0c551968bc681141f06bcf13debeb6163836dec2
Cristy <villa.cm@mobilityxx2023.onmicrosoft.com>
2025-02-03 14:30:30 +0800

    [test] add console.log

--- index.js
+++ index.js
@@ -1,72 +1,68 @@
-
-
-
 // MY INTERESTS function:
 
-var slides = document.querySelectorAll('.slide');
-var btns = document.querySelectorAll('.btn');
-var slideavatars = document.querySelectorAll('.slideavatar');
-var slidetexts = document.querySelectorAll('.slidetext');
-var buttonlinks = document.querySelectorAll('.buttonlink');
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".btn");
+var slideavatars = document.querySelectorAll(".slideavatar");
+var slidetexts = document.querySelectorAll(".slidetext");
+var buttonlinks = document.querySelectorAll(".buttonlink");
 let currentSlide = 1;
 
 // Javascript for image slider manual navigation
-var manualNav = function(manual){
+var manualNav = function (manual) {
   slides.forEach((slide) => {
-    slide.classList.remove('active');
+    slide.classList.remove("active");
 
     btns.forEach((btn) => {
-      btn.classList.remove('active');
-        });
+      btn.classList.remove("active");
+    });
     slideavatars.forEach((slideavatar) => {
-      slideavatar.classList.remove('active');
-        });
+      slideavatar.classList.remove("active");
+    });
     slidetexts.forEach((slidetext) => {
-      slidetext.classList.remove('active');
-        });
+      slidetext.classList.remove("active");
+    });
     buttonlinks.forEach((buttonlink) => {
-      buttonlink.classList.remove('active');
-        });
-
+      buttonlink.classList.remove("active");
+    });
   });
 
-  slides[manual].classList.add('active');
-  btns[manual].classList.add('active');
-  slideavatars[manual].classList.add('active');
-  slidetexts[manual].classList.add('active');
-  buttonlinks[manual].classList.add('active');
-}
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+  slideavatars[manual].classList.add("active");
+  slidetexts[manual].classList.add("active");
+  buttonlinks[manual].classList.add("active");
+};
 
 btns.forEach((btn, i) => {
   btn.addEventListener("click", () => {
     manualNav(i);
     currentSlide = i;
-      });
+  });
 });
 
-
 // music----------------
-var music= document.getElementById("music");
+var music = document.getElementById("music");
 var icon = document.getElementById("play");
 
-icon.onclick =function(){
-  if(music.paused){
+icon.onclick = function () {
+  if (music.paused) {
     music.play();
-    icon.src="./svg/pause.svg";
-  }
-  else{
+    icon.src = "./svg/pause.svg";
+  } else {
     music.pause();
-    icon.src="./svg/play.svg";
+    icon.src = "./svg/play.svg";
   }
-  
-}
+};
 
 // preloader-----------------
-window.addEventListener('load', ()=>{
-  const preload=document.querySelector('.loader');
-  preload.classList.add('preload-finish');
-})
-
+window.addEventListener("load", () => {
+  const preload = document.querySelector(".loader");
+  preload.classList.add("preload-finish");
+});
+console.log(
+  "IS USING DEKSTOP",
+  !/android|iphone|ipad/i.test(navigator.userAgent)
+);
 /*window.addEventListener('load', ()=>{
   const preload=document.querySelector('.loader');
   loader.classList.add('preload-finish');
